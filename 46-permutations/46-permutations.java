769d3ec8d92fdb94a65137614b0634ebbb71a146
jayathecoder <89962883+jayathecoder@users.noreply.github.com>
2022-07-23 23:05:49 +0530

    Time: 2 ms (76.41%), Space: 44.6 MB (53.79%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,7 +1,7 @@
 class Solution {
     
     
-    public static void permutations(int[] nums,boolean[] check,List<Integer> ds,List<List<Integer>> result){
+  /*  public static void permutations(int[] nums,boolean[] check,List<Integer> ds,List<List<Integer>> result){
         
         if(ds.size()==nums.length){
             result.add(new ArrayList(ds));
@@ -30,6 +30,34 @@ class Solution {
         permutations(nums,check,ds,result);
         
         return result;
+        
+        */
+    
+     private void recurPermute(int index, int[] nums, List < List < Integer >> ans) {
+        if (index == nums.length) {
+            // copy the ds to ans
+            List < Integer > ds = new ArrayList < > ();
+            for (int i = 0; i < nums.length; i++) {
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList < > (ds));
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            recurPermute(index + 1, nums, ans);
+            swap(i, index, nums);
+        }
+    }
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+    public List < List < Integer >> permute(int[] nums) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        recurPermute(0, nums, ans);
+        return ans;
         
     }
 }
