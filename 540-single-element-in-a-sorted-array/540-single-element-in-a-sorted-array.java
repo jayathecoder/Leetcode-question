d44d9f85ea6c4f546b2e1874bbbb287a7b22c128
jayathecoder <89962883+jayathecoder@users.noreply.github.com>
2022-07-25 23:45:06 +0530

    Time: 0 ms (100.00%), Space: 60.4 MB (55.98%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -4,25 +4,32 @@ class Solution {
         int high = nums.length - 2;
         
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (mid % 2 == 0) {
-                // Checking whether we are in right half
-                if (nums[mid] != nums[mid + 1]) 
+//             int mid = (low + high) / 2;
+//             if (mid % 2 == 0) {
+//                 // Checking whether we are in right half
+//                 if (nums[mid] != nums[mid + 1]) 
                     
-                    //Shrinking the right half
-                    high = mid - 1; 
-                else
-                    // Shrinking the left half
-                    low = mid + 1; 
-            } else {
-                // Checking whether we are in right half
-                if (nums[mid] == nums[mid + 1])
-                    //Shrinking the right half
-                    high = mid - 1; 
-                else
-                    // Shrinking the left half
-                    low = mid + 1; 
-            }
+//                     //Shrinking the right half
+//                     high = mid - 1; 
+//                 else
+//                     // Shrinking the left half
+//                     low = mid + 1; 
+//             } else {
+//                 // Checking whether we are in right half
+//                 if (nums[mid] == nums[mid + 1])
+//                     //Shrinking the right half
+//                     high = mid - 1; 
+//                 else
+//                     // Shrinking the left half
+//                     low = mid + 1; 
+//             }
+//         }
+            
+            int mid=(low+high)>>1;
+            if(nums[mid]==nums[mid^1])
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return nums[low];
     }
