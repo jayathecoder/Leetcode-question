577f3c3c94cac1405658e2b5b5804a33045ecf1d
jayathecoder <89962883+jayathecoder@users.noreply.github.com>
2022-09-05 23:30:23 +0530

    Time: 0 ms (100.00%), Space: 41 MB (41.77%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -11,9 +11,16 @@ class Solution {
     }
     
     public int climbStairs(int n) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int result=helper(n,map);
-        return result;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // int result=helper(n,map);
+        int dp[]=new int[50];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
         
     }
 }
