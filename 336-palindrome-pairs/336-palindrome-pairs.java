b1698365e99767df0b594ff67662defabaf793ca
jayathecoder <89962883+jayathecoder@users.noreply.github.com>
2022-09-17 23:17:09 +0530

    Time: 2119 ms (19.08%), Space: 256.4 MB (52.03%) - LeetHub

---
+++ 336-palindrome-pairs/336-palindrome-pairs.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(String s){
+        int j=s.length()-1;
+        String str=s;
+        for(int i=0;i<s.length() && i<j;i++){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            j--;
+        }
+       return true;
+    }
+    public List<List<Integer>> palindromePairs(String[] words) {
+     List<List<Integer>> ans = new ArrayList<>();
+Map<String, Integer> map = new HashMap<>(); // {reversed word: its index}
+
+for (int i = 0; i < words.length; ++i)
+  map.put(new StringBuilder(words[i]).reverse().toString(), i);
+
+for (int i = 0; i < words.length; ++i) {
+  final String word = words[i];
+  // special case to prevent duplicate calculation
+  if (map.containsKey("") && map.get("") != i && isPalindrome(word))
+    ans.add(Arrays.asList(i, map.get("")));
+  for (int j = 1; j <= word.length(); ++j) {
+    final String l = word.substring(0, j);
+    final String r = word.substring(j);
+    if (map.containsKey(l) && map.get(l) != i && isPalindrome(r))
+      ans.add(Arrays.asList(i, map.get(l)));
+    if (map.containsKey(r) && map.get(r) != i && isPalindrome(l))
+      ans.add(Arrays.asList(map.get(r), i));
+  }
+}
+
+return ans;
+    }
+}
