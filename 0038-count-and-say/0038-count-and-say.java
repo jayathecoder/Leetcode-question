a11d40a5e16d6e229f0e3546411ad62117e6867e
JAYA VISHWAKARMA <89962883+jayathecoder@users.noreply.github.com>
2022-10-21 11:11:57 +0530

    Time: 10 ms (45.93%), Space: 41.6 MB (74.23%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,29 +1,20 @@
 class Solution {
     public String countAndSay(int n) {
-        String s = "1";
-        for(int i=2;i<=n;i++){
-            s = countIndex(s);
-        }
-        return s;
-    }
-    
-    public String countIndex(String s){
-        StringBuilder sb = new StringBuilder();
-        char c = s.charAt(0);
-        int count = 1;
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i) == c)
-                count++;
-            else{
-                sb.append(count);
-                sb.append(c);
-                c = s.charAt(i);
-                count=1;
+          StringBuilder currStr = new StringBuilder("1");
+
+        for (int k = 0; k < n - 1; k++) {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for (int i = 1; i < currStr.length(); i++) {
+                if (currStr.charAt(i) == currStr.charAt(i - 1)) count++;
+                else{
+                    sb.append(count).append(currStr.charAt(i - 1));
+                    count = 1;
+                }
             }
+            sb.append(count).append(currStr.charAt(currStr.length()-1));
+            currStr = sb;
         }
-        sb.append(count);
-        sb.append(c);
-        return sb.toString();
-        
+        return currStr.toString();
     }
 }
