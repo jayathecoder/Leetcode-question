f34c8cdec2b8dd1dc00abd6c0cf9e7aae0ba9f89
JAYA VISHWAKARMA <89962883+jayathecoder@users.noreply.github.com>
2022-10-23 19:16:34 +0530

    Time: 2 ms (97.99%), Space: 54.5 MB (64.15%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -6,11 +6,10 @@ class Solution {
         for(int i=0;i<n;i++){
             temp[nums[i]]++;
         }
-        for(int i=1;i<=n;i++){
-            if(temp[i]==2){
+        for(int i=1;i<=n;i++)
+            if(temp[i]==2)
                 result[0]=i;
-            }
-        }
+            
         for(int i=1;i<=n;i++)
             if(temp[i]==0) result[1]=i;
         return result;
