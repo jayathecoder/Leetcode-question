6420febfc9e86363805d1c9b9532d8882961d8a4
JAYA VISHWAKARMA <89962883+jayathecoder@users.noreply.github.com>
2022-11-08 12:49:32 +0530

    Time: 5 ms (85.94%), Space: 44.8 MB (37.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        char a1[]=s.toCharArray();
+        char a1[]=s.toCharArray();//approach 1 takes O(nlogn)
         Arrays.sort(a1);
         s=new String(a1);
         char a2[]=t.toCharArray();
@@ -9,5 +9,5 @@ class Solution {
         t=new String(a2);
         if(s.equals(t)) return true;
         else return false;
-    }
+    }//approach 2 will take O(n) which is to use frequency array
 }
